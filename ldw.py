28bbbfc710da9a0c6ec8d7852feeeaa49dedc500
Michael Mikhalevich <mike@viber.com>
2018-05-23 13:09:59 +0300

    "Not found" libraries search was implemented

--- ldw.py
+++ ldw.py
@@ -22,44 +22,75 @@ def is_executable(filename):
     return False
 
 
+def ldd_deps(filename):
+    """return list with dependencies and not found dependencies for current shared object
+    without address which is returned by ldd command"""
+    re_remove_address_pattern = re.compile(r"\s*\(.+\)\s*$", re.MULTILINE)
+    re_pattern = re.compile(r"^\s*(.+)\s*$", re.MULTILINE)
+    not_found_pattern = re.compile(r"not found", re.IGNORECASE)
+
+    deps_list = []
+    not_found_list = []
+
+    output = execute_shell("ldd {0}".format(filename))
+    output = re_remove_address_pattern.sub("", output)
+
+    for match in re_pattern.finditer(output):
+        if match:
+            dep = match.group(1)
+            if not_found_pattern.search(dep):
+                not_found_list.append(dep)
+            deps_list.append(dep)
+
+    return deps_list, not_found_list
+
+
 def collect_dependencies(root_dir):
     """collect all dependencies, returns tuple where
-    first value it's dictionary where key - library or executable value - list of dependencies
-    second value it's set with unique library dependencies"""
+    first value it's dictionary with not found dependencies
+    second value it's dictionary where key - library or executable value - list of dependencies
+    third value it's set with unique library dependencies"""
+    not_found_deps = {}
     group_deps = {}
     unique_deps = set()
-    re_pattern = re.compile(r"^(.+)\s+\(.*\)$", re.MULTILINE)
     for root, dirs, files in os.walk(root_dir):
         for file in files:
             filename = os.path.abspath(os.path.join(root, file))
             if is_executable(filename):
-                output = execute_shell("ldd {0}".format(filename))
-                deps = []
-                for match in re_pattern.finditer(output):
-                    if match:
-                        dep = match.group(1)
-                        deps.append(dep)
-                        index = dep.find("=>")
-                        if index >= 0:
-                            dep = dep[0:index]
-                        unique_deps.add(dep)
-                if len(deps) > 0:
-                    group_deps[filename] = deps
+                deps_list, not_found_list = ldd_deps(filename)
+                if len(not_found_list) > 0:
+                    not_found_deps[filename] = not_found_list
+                if len(deps_list) > 0:
+                    group_deps[filename] = deps_list
 
-    return group_deps, unique_deps
+                for dep in deps_list:
+                    index = dep.find("=>")
+                    if index >= 0:
+                        dep = dep[0:index]
+                    unique_deps.add(dep)
+
+    return not_found_deps, group_deps, unique_deps
 
 
-def print_results(group_deps, unique_deps, all_flag, group_flag):
+def print_results(not_found_list, group_deps, unique_deps, all_flag, group_flag):
     if group_flag:
         for lib, deps in group_deps.items():
-            print("dependencies for {0}: ================================>".format(lib))
+            print("\ndependencies for {0}: ================================>".format(lib))
             for dep in sorted(deps):
                 print("\t\t{0}".format(dep))
 
     if all_flag:
-        print("all dependencies: ================================>")
+        print("\nall dependencies: ================================>")
         for dep in sorted(unique_deps):
             print("\t\t{0}".format(dep))
+
+    if len(not_found_list) > 0:
+        for lib, deps in not_found_list.items():
+            print("\nnot found dependencies for {0}: ================================>".format(lib))
+            for dep in sorted(deps):
+                print("\t\t{0}".format(dep))
+    else:
+        print("\nNo missing libs found...")
 
 
 def main():
@@ -72,8 +103,8 @@ def main():
 
     start_time = time.time()
 
-    group_deps, unique_deps = collect_dependencies(args.root)
-    print_results(group_deps, unique_deps, args.all, args.group)
+    not_found_deps, group_deps, unique_deps = collect_dependencies(args.root)
+    print_results(not_found_deps, group_deps, unique_deps, args.all, args.group)
 
     end_time = time.time()
     print("script execution: {0} ms".format((end_time - start_time) * 1000))
